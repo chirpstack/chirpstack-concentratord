5d99586dfe70710fafb847c5796166a04455a166
Orne Brocaar <info@brocaar.com>
2020-03-22 20:50:39 +0100

    Implement RAK2246 model configuration.

---
+++ chirpstack-concentratord-sx1301/src/config/vendor/rak/mod.rs
@@ -0,0 +1,9 @@
+pub mod rak2246_as923;
+pub mod rak2246_au915;
+pub mod rak2246_cn470;
+pub mod rak2246_eu433;
+pub mod rak2246_eu868;
+pub mod rak2246_in865;
+pub mod rak2246_kr920;
+pub mod rak2246_ru864;
+pub mod rak2246_us915;
