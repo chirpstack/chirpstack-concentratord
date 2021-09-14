9c23da6b4b9a4f9307e2bd564b3f75723ae75005
Orne Brocaar <info@brocaar.com>
2021-09-14 13:24:52 +0100

    Add imst_ic880a_ru864 configuration.

---
+++ chirpstack-concentratord-sx1301/src/config/vendor/imst/ic880a_ru864.rs
@@ -0,0 +1,155 @@
+use libloragw_sx1301::hal;
+
+use super::super::super::super::config;
+use super::super::Configuration;
+
+// source:
+// https://shop.imst.de/media/pdf/f5/68/7f/WiMOD_LiteGateway_QuickStartGuide_V1_5.pdf
+pub fn new(conf: &config::Configuration) -> Configuration {
+    Configuration {
+        radio_count: 2,
+        clock_source: 1,
+        radio_rssi_offset: vec![-166.0, -166.0],
+        radio_tx_enabled: vec![true, false],
+        radio_type: vec![hal::RadioType::SX1257, hal::RadioType::SX1257],
+        radio_min_max_tx_freq: vec![(864000000, 870000000), (864000000, 870000000)],
+        radio_tx_notch_freq: vec![0, 0],
+        lora_multi_sf_bandwidth: 125000,
+        tx_gain_table: vec![
+            // 0
+            hal::TxGainConfig {
+                pa_gain: 0,
+                mix_gain: 8,
+                rf_power: -6,
+                dig_gain: 2,
+                dac_gain: 3,
+            },
+            // 1
+            hal::TxGainConfig {
+                pa_gain: 0,
+                mix_gain: 11,
+                rf_power: -3,
+                dig_gain: 3,
+                dac_gain: 3,
+            },
+            // 2
+            hal::TxGainConfig {
+                pa_gain: 0,
+                mix_gain: 10,
+                rf_power: 0,
+                dig_gain: 0,
+                dac_gain: 3,
+            },
+            // 3
+            hal::TxGainConfig {
+                pa_gain: 0,
+                mix_gain: 14,
+                rf_power: 3,
+                dig_gain: 1,
+                dac_gain: 3,
+            },
+            // 4
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 8,
+                rf_power: 6,
+                dig_gain: 3,
+                dac_gain: 3,
+            },
+            // 5
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 8,
+                rf_power: 10,
+                dig_gain: 2,
+                dac_gain: 3,
+            },
+            // 6
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 8,
+                rf_power: 11,
+                dig_gain: 1,
+                dac_gain: 3,
+            },
+            // 7
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 9,
+                rf_power: 12,
+                dig_gain: 2,
+                dac_gain: 3,
+            },
+            // 8
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 11,
+                rf_power: 13,
+                dig_gain: 3,
+                dac_gain: 3,
+            },
+            // 9
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 10,
+                rf_power: 14,
+                dig_gain: 2,
+                dac_gain: 3,
+            },
+            // 10
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 11,
+                rf_power: 16,
+                dig_gain: 2,
+                dac_gain: 3,
+            },
+            // 11
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 12,
+                rf_power: 20,
+                dig_gain: 0,
+                dac_gain: 3,
+            },
+            // 12
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 12,
+                rf_power: 23,
+                dig_gain: 0,
+                dac_gain: 3,
+            },
+            // 13
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 12,
+                rf_power: 25,
+                dig_gain: 0,
+                dac_gain: 3,
+            },
+            // 14
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 12,
+                rf_power: 26,
+                dig_gain: 0,
+                dac_gain: 3,
+            },
+            // 15
+            hal::TxGainConfig {
+                pa_gain: 2,
+                mix_gain: 12,
+                rf_power: 27,
+                dig_gain: 0,
+                dac_gain: 3,
+            },
+        ],
+        gps_tty_path: None,
+        spidev_path: "/dev/spidev0.0".to_string(),
+        reset_pin: match conf.gateway.reset_pin {
+            0 => Some((0, 5)),
+            _ => Some((0, conf.gateway.reset_pin)),
+        },
+    }
+}
--- chirpstack-concentratord-sx1301/src/config/vendor/imst/mod.rs
+++ chirpstack-concentratord-sx1301/src/config/vendor/imst/mod.rs
@@ -1,2 +1,3 @@
 pub mod ic880a_eu868;
 pub mod ic880a_in865;
+pub mod ic880a_ru864;
