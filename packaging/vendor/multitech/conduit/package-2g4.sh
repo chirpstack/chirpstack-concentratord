a4d7c721a2c58a19231a38db975c26a260589a49
Orne Brocaar <info@brocaar.com>
2022-12-06 10:45:20 +0000

    Update Multitech Conduit packaging (sx1301 + 2g4) with Monit config.

--- packaging/vendor/multitech/conduit/package-2g4.sh
+++ packaging/vendor/multitech/conduit/package-2g4.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 PACKAGE_NAME="chirpstack-concentratord-2g4"
 PACKAGE_VERSION=$1
 REV="r1"
@@ -26,19 +28,35 @@ Description: ChirpStack Concentratord 2g4
 EOF
 
 cat > $PACKAGE_DIR/CONTROL/postinst << EOF
+sed -i "s/ENABLED=.*/ENABLED=\"yes\"/" /etc/default/monit
+update-rc.d monit defaults
+/etc/init.d/monit start
+/usr/bin/monit reload
 EOF
 chmod 755 $PACKAGE_DIR/CONTROL/postinst
 
+cat > $PACKAGE_DIR/CONTROL/prerm << EOF
+/etc/init.d/$PACKAGE_NAME-ap1 stop
+/etc/init.d/$PACKAGE_NAME-ap2 stop
+EOF
+chmod 755 $PACKAGE_DIR/CONTROL/prerm
+
+# This is empty, because the config files are copied on start.
 cat > $PACKAGE_DIR/CONTROL/conffiles << EOF
 EOF
 
 # Files
 mkdir -p $PACKAGE_DIR/opt/$PACKAGE_NAME
 mkdir -p $PACKAGE_DIR/var/config/$PACKAGE_NAME/examples
+mkdir -p $PACKAGE_DIR/etc/monit.d
 mkdir -p $PACKAGE_DIR/etc/init.d
 
 cp files/2g4/$PACKAGE_NAME-ap1.init $PACKAGE_DIR/etc/init.d/$PACKAGE_NAME-ap1
 cp files/2g4/$PACKAGE_NAME-ap2.init $PACKAGE_DIR/etc/init.d/$PACKAGE_NAME-ap2
+
+cp files/2g4/$PACKAGE_NAME-ap1.monit $PACKAGE_DIR/var/config/$PACKAGE_NAME/examples
+cp files/2g4/$PACKAGE_NAME-ap2.monit $PACKAGE_DIR/var/config/$PACKAGE_NAME/examples
+
 cp files/2g4/*.toml $PACKAGE_DIR/var/config/$PACKAGE_NAME/examples/
 cp $BIN_PATH $PACKAGE_DIR/opt/$PACKAGE_NAME/$PACKAGE_NAME
 
--- packaging/vendor/multitech/conduit/package-sx1301.sh
+++ packaging/vendor/multitech/conduit/package-sx1301.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 PACKAGE_NAME="chirpstack-concentratord-sx1301"
 PACKAGE_VERSION=$1
 REV="r1"
@@ -26,20 +28,36 @@ Description: ChirpStack Concentratord SX1301
 EOF
 
 cat > $PACKAGE_DIR/CONTROL/postinst << EOF
+sed -i "s/ENABLED=.*/ENABLED=\"yes\"/" /etc/default/monit
+update-rc.d monit defaults
+/etc/init.d/monit start
+/usr/bin/monit reload
 EOF
 chmod 755 $PACKAGE_DIR/CONTROL/postinst
 
+cat > $PACKAGE_DIR/CONTROL/prerm << EOF
+/etc/init.d/$PACKAGE_NAME-ap1 stop
+/etc/init.d/$PACKAGE_NAME-ap2 stop
+EOF
+chmod 755 $PACKAGE_DIR/CONTROL/prerm
+
+# This is empty, because the config files are copied on start.
 cat > $PACKAGE_DIR/CONTROL/conffiles << EOF
 EOF
 
 # Files
 mkdir -p $PACKAGE_DIR/opt/$PACKAGE_NAME
 mkdir -p $PACKAGE_DIR/var/config/$PACKAGE_NAME/examples
+mkdir -p $PACKAGE_DIR/etc/monit.d
 mkdir -p $PACKAGE_DIR/etc/init.d
 
 cp files/sx1301/$PACKAGE_NAME-ap1.init $PACKAGE_DIR/etc/init.d/$PACKAGE_NAME-ap1
 cp files/sx1301/$PACKAGE_NAME-ap2.init $PACKAGE_DIR/etc/init.d/$PACKAGE_NAME-ap2
-cp files/sx1301/*.toml $PACKAGE_DIR/var/config/$PACKAGE_NAME/examples/
+
+cp files/sx1301/$PACKAGE_NAME-ap1.monit $PACKAGE_DIR/var/config/$PACKAGE_NAME/examples
+cp files/sx1301/$PACKAGE_NAME-ap2.monit $PACKAGE_DIR/var/config/$PACKAGE_NAME/examples
+
+cp files/sx1301/*.toml $PACKAGE_DIR/var/config/$PACKAGE_NAME/examples
 cp $BIN_PATH $PACKAGE_DIR/opt/$PACKAGE_NAME/$PACKAGE_NAME
 
 # Package
