efaa1b7baab33f79d36a711ac7e7a24c61801edd
Orne Brocaar <info@brocaar.com>
2024-10-03 15:30:08 +0100

    Configure NAV-TIMEGPS if Gpsd is used.

--- libconcentratord/src/gpsd.rs
+++ libconcentratord/src/gpsd.rs
@@ -3,6 +3,18 @@ use std::net::TcpStream;
 
 use anyhow::Result;
 use log::{debug, info};
+use serde::Deserialize;
+
+#[derive(Debug, Clone, Deserialize)]
+struct DevicesResponse {
+    pub devices: Vec<DevicesResponseDevice>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct DevicesResponseDevice {
+    pub path: String,
+    pub driver: String,
+}
 
 pub fn get_reader(server: &str) -> Result<BufReader<TcpStream>> {
     info!("Connecting to gpsd, server: {}", server);
@@ -22,12 +34,23 @@ pub fn get_reader(server: &str) -> Result<BufReader<TcpStream>> {
     // DEVICES
     let mut b = Vec::new();
     reader.read_until(b'\n', &mut b)?;
+    let resp: DevicesResponse = serde_json::from_slice(&b)?;
     debug!("Devices response: {}", String::from_utf8(b.clone())?);
 
     // WATCH
     let mut b = Vec::new();
     reader.read_until(b'\n', &mut b)?;
     debug!("Watch response: {}", String::from_utf8(b.clone())?);
+
+    for device in &resp.devices {
+        if device.driver == "u-blox" {
+            debug!("Configuring uBlox device {} for NAV-TIMEGPS", device.path);
+            writer.write_all(
+                format!("&{}=b5620601080001200001010000003294", device.path).as_bytes(),
+            )?;
+            writer.flush()?;
+        }
+    }
 
     Ok(reader)
 }
